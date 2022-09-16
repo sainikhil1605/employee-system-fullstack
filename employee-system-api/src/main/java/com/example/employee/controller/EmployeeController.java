c77237cbc2b11aa462cbef7020ae6e8ff63d06f8
Nikhil Reddy <35322884+sainikhil1605@users.noreply.github.com>
2022-09-16 11:36:16 -0500

    Added Get Employees Functionality

--- employee-system-api/src/main/java/com/example/employee/controller/EmployeeController.java
+++ employee-system-api/src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package com.example.employee.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.employee.model.Employee;
 import com.example.employee.services.EmployeeService;
 
+@CrossOrigin(originPatterns = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -21,5 +26,9 @@ public class EmployeeController {
 	@PostMapping("employees")
 	public Employee creatEmployee(@RequestBody Employee employee) {
 		return employeeService.createEmployee(employee);
+	}
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeService.getAllEmployees();
 	}
 }
--- employee-system-api/src/main/java/com/example/employee/services/EmployeeService.java
+++ employee-system-api/src/main/java/com/example/employee/services/EmployeeService.java
@@ -1,7 +1,10 @@
 package com.example.employee.services;
 
+import java.util.List;
+
 import com.example.employee.model.Employee;
 
 public interface EmployeeService {
 	Employee createEmployee(Employee employee);
+	List<Employee> getAllEmployees();
 }
